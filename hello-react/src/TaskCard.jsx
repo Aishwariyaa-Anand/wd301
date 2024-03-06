c30a7b2311a977c4874039dc814cdfa66a866e1a
Aishwariyaa_Anand <aishwariyaa.anand@gmail.com>
2024-03-06 15:37:15 +0530

    layout changed to tasks

---
+++ hello-react/src/TaskCard.jsx
@@ -0,0 +1,11 @@
+const TaskCard = () => {
+    return (
+        <div>
+            <h2>title</h2>
+            <p>completed on</p>
+            <p>assignee</p>
+        </div>
+    )
+}
+
+export default TaskCard
