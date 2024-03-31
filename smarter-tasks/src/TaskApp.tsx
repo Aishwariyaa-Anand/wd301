1e934f799d2f9d7c229adf4768b1907a133aac5a
Aishwariyaa_Anand <aishwariyaa.anand@gmail.com>
2024-03-31 21:07:27 +0530

    lvl 4 removed FCs

--- smarter-tasks/src/TaskApp.tsx
+++ smarter-tasks/src/TaskApp.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { TaskItem } from "./types";
-import TaskForm from "./TaskFormFC";
-import TaskList from "./TaskListFC";
+import TaskForm from "./TaskForm";
+import TaskList from "./TaskList";
 import { useLocalStorage } from "./hooks/useLocalStorage";
 
 //interface TaskAppProp {}
