556fb57a42c83e1513617adae80893c5786e12ac
Aishwariyaa_Anand <aishwariyaa.anand@gmail.com>
2024-04-24 16:22:43 +0530

    lvl 7 application state modelling

--- smarter-tasks/src/pages/projects/NewProject.tsx
+++ smarter-tasks/src/pages/projects/NewProject.tsx
@@ -1,55 +1,58 @@
+// src/pages/projects/NewProject.tsx
 import { Dialog, Transition } from '@headlessui/react'
-import { API_ENDPOINT } from '../../config/constants';
-import { useForm, SubmitHandler } from "react-hook-form";
 import { Fragment, useState } from 'react'
+import { useForm, SubmitHandler } from "react-hook-form";
+
+// First I'll import the addProject function
+import { addProject } from '../../context/projects/actions';
+
+// Then I'll import the useProjectsDispatch hook from projects context
+import { useProjectsDispatch } from "../../context/projects/context";
 type Inputs = {
-    name: string
-  };
-
-
+  name: string
+};
 const NewProject = () => {
-  let [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false)
+
+  // Next, I'll add a new state to handle errors.
+  const [error, setError] = useState(null)
+
+  // Then I'll call the useProjectsDispatch function to get the dispatch function 
+  // for projects 
+  const dispatchProjects = useProjectsDispatch();
   const { register, handleSubmit, formState: { errors } } = useForm<Inputs>();
-  const openModal = () => {
-    setIsOpen(true)
-  }
- 
   const closeModal = () => {
     setIsOpen(false)
   }
-
-  const [name, setName] = useState('');
-
+  const openModal = () => {
+    setIsOpen(true)
+  }
   const onSubmit: SubmitHandler<Inputs> = async (data) => {
     const { name } = data
-    const token = localStorage.getItem("authToken") ?? "";
-    try {
-      const response = await fetch(`${API_ENDPOINT}/projects`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json', "Authorization": `Bearer ${token}` },
-        body: JSON.stringify({ name }),
-      }); 
-      if (!response.ok) {
-        throw new Error('Failed to create project');
-      } 
-      const data = await response.json();
- 
-      console.log(data)
-      setIsOpen(false)
-    } catch (error) { 
-      console.error('Operation failed:', error);
-    }
-  } 
 
+    // Next, I'll call the addProject function with two arguments: 
+    //`dispatchProjects` and an object with `name` attribute. 
+    // As it's an async function, we will await for the response.
+    const response = await addProject(dispatchProjects, { name })
+
+    // Then depending on response, I'll either close the modal...
+    if (response.ok) {
+      setIsOpen(false)
+    } else {
+
+      // Or I'll set the error.
+      setError(response.error as React.SetStateAction<null>)
+    }
+  };
   return (
     <>
       <button
-            type="button"
-            onClick={openModal}
-            className="rounded-md bg-blue-600 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-95 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
-        >
-            New Project
-        </button>
+        type="button"
+        onClick={openModal}
+        className="rounded-md bg-blue-600 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-95 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+      >
+        New Project
+      </button>
       <Transition appear show={isOpen} as={Fragment}>
         <Dialog as="div" className="relative z-10" onClose={closeModal}>
           <Transition.Child
@@ -75,32 +78,36 @@ const NewProject = () => {
                 leaveTo="opacity-0 scale-95"
               >
                 <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-                    <Dialog.Title
+                  <Dialog.Title
                     as="h3"
                     className="text-lg font-medium leading-6 text-gray-900"
-                    >
+                  >
                     Create new project
-                    </Dialog.Title>
-                    <div className="mt-2">
-                    <form  onSubmit={handleSubmit(onSubmit)}>
-                        <input
-                            type="text"
-                            placeholder='Enter project name...'
-                            autoFocus
-                            {...register('name', { required: true })}
-                            className={`w-full border rounded-md py-2 px-3 my-4 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue ${
-                              errors.name ? 'border-red-500' : ''
-                            }`}
-                        />
-                        {errors.name && <span>This field is required</span>}
-                        <button type="submit" className="inline-flex justify-center rounded-md border border-transparent bg-blue-600 px-4 py-2 mr-2 text-sm font-medium text-white hover:bg-blue-500 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2">
-                            Submit
-                        </button>
-                        <button type="submit" onClick={closeModal} className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2">
-                            Cancel
-                        </button>
+                  </Dialog.Title>
+                  <div className="mt-2">
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                      {/* I'll show the error, if it exists.*/}
+                      {error &&
+                        <span>{error}</span>
+                      }
+                      <input
+                        type="text"
+                        placeholder='Enter project name...'
+                        autoFocus
+                        {...register('name', { required: true })}
+                        className={`w-full border rounded-md py-2 px-3 my-4 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue ${
+                          errors.name ? 'border-red-500' : ''
+                        }`}
+                      />
+                      {errors.name && <span>This field is required</span>}
+                      <button type="submit" className="inline-flex justify-center rounded-md border border-transparent bg-blue-600 px-4 py-2 mr-2 text-sm font-medium text-white hover:bg-blue-500 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2">
+                        Submit
+                      </button>
+                      <button type="submit" onClick={closeModal} className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2">
+                        Cancel
+                      </button>
                     </form>
-                    </div>
+                  </div>
                 </Dialog.Panel>
               </Transition.Child>
             </div>
@@ -110,5 +117,4 @@ const NewProject = () => {
     </>
   )
 }
-
 export default NewProject;
